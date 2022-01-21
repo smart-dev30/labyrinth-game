14b9a71b2115fbc9d843774e486d1688e92d90bf
Lucas Assmann <l.assmann.30@gmail.com>
2022-01-21 14:27:54 -0300

    Puzzle challenge

---
+++ src/Labyrinth.test.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import { Labyrinth } from "./solution/Labyrinth";
+import { Props } from "./solution/Labyrinth/Labyrinth";
+
+describe("Labyrinth", () => {
+  let props: Props;
+  beforeEach(() => {
+    props = {
+
+    };
+  });
+
+  it("should win", () => {
+
+  });
+
+  it("should lose", () => {
+
+  });
+});
